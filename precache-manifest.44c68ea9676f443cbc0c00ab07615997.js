3d4d49d69e9bed6965b54031bf5ac1abeb0423bf
MdAnowarHossenFaysal <65511343+MdAnowarHossenFaysal@users.noreply.github.com>
2020-10-03 01:50:14 +0600

    Add files via upload

---
+++ precache-manifest.44c68ea9676f443cbc0c00ab07615997.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dceb30ab93388e1694ffbe5348b67bd3",
+    "url": "/Covid19Live/index.html"
+  },
+  {
+    "revision": "4a0009b989a49684f8e3",
+    "url": "/Covid19Live/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "89ef23a127b9aa2dddd7",
+    "url": "/Covid19Live/static/js/2.4294c354.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Covid19Live/static/js/2.4294c354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0009b989a49684f8e3",
+    "url": "/Covid19Live/static/js/main.1770e40a.chunk.js"
+  },
+  {
+    "revision": "6b8394aff1b51739702d",
+    "url": "/Covid19Live/static/js/runtime-main.0a1b0878.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Covid19Live/static/media/logo.5d5d9eef.svg"
+  }
+]);
